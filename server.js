dd3a8fb76cc9671ee183af9937ccca6a09b23654
SegeonKim <segeon215@naver.com>
2017-05-07 18:30:24 +0900

    add connect-route

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var connect = require('connect');
+var connectRoute = require('connect-route');
 var finalhandler = require('finalhandler');
 var http  = require('http');
 var bodyParser = require('body-parser');
@@ -10,10 +11,12 @@ var app = connect();
 
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: false}));
-// app.use('/', function(req, res) {
-//   res.setHeader('Content-Type', 'text/plain; charset=utf-8');
-//   res.end('Hello World!');
-// });
-app.use('/transcribe', dash.transcribe);
+app.use(connectRoute(function(router) {
+  router.get('/', function(req, res) {
+    res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+    res.end('Hello World!');
+  });
+  router.post('/transcribe', dash.transcribe);
+}));
 
 http.createServer(app).listen(PORT);
