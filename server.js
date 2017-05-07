3cf7102d1aa268d22fd079d779dee3f3152eacc5
SegeonKim <segeon215@naver.com>
2017-05-07 18:16:25 +0900

    use connect mid ware

--- server.js
+++ server.js
@@ -1,23 +1,24 @@
+var connect = require('connect');
 var finalhandler = require('finalhandler');
 var http  = require('http');
 var bodyParser = require('body-parser');
-var Router = require('router');
 var dash = require('./dash.js');
-var jsonParser = bodyParser.json();
 
 var PORT = 8080;
 
-var router = Router();
-router.get('/', function (req, res) {
-	res.setHeader('Content-Type', 'text/plain; charset=utf-8');
-	res.end('Hello World!');
-});
-router.get('/test', dash.test);
-router.post('/transcribe', jsonParser, dash.transcribe);
+var app = connect();
 
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: false}));
 
-var server = http.createServer(function(req, res) {
-	router(req, res, finalhandler(req, res));
-});
+server.use(connect.router(function (router){
+    router.get('/', function(request, response){
+      res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+      res.end('Hello World!');
+    });
 
-server.listen(PORT);
+    router.post('/transcribe', dash.transcribe);
+  })
+);
+
+http.createServer(app).listen(PORT);
