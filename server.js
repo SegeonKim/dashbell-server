dac6024f381b6b6d9563439fae33b36f3d92770a
SegeonKim <segeon215@naver.com>
2017-05-07 18:44:49 +0900

    fix router

--- server.js
+++ server.js
@@ -4,19 +4,22 @@ var finalhandler = require('finalhandler');
 var http  = require('http');
 var bodyParser = require('body-parser');
 var dash = require('./dash.js');
+var Router = require('router');
 
 var PORT = 8080;
 
 var app = connect();
+var router = Router();
 
-var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: false}));
-app.use(connectRoute(function(router) {
-  router.get('/', function(req, res) {
-    res.setHeader('Content-Type', 'text/plain; charset=utf-8');
-    res.end('Hello World!');
-  });
-  router.post('/transcribe', dash.transcribe);
-}));
+app.use(bodyParser.json());
+app.use(function(req, res) {
+	router(req, res, finalhandler(req, res));
+});
+
+router.get('/', function(req, res) {
+  res.end('test');
+});
+router.get('/transcribe', dash.transcribe);
 
 http.createServer(app).listen(PORT);
